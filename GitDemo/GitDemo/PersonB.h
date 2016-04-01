6c37c5dce117bd3debe303fe66eb8d6211f1fb3d
WangZhuoYue <1584916804@qq.com>
2016-04-01 15:05:58 +0800

    创建B类

---
+++ GitDemo/GitDemo/PersonB.h
@@ -0,0 +1,13 @@
+//
+//  PersonB.h
+//  GitDemo
+//
+//  Created by qianfeng on 16/4/1.
+//  Copyright © 2016年 王卓越. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PersonB : NSObject
+
+@end
